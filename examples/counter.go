aed88339a59da72ff0781d94554e3ce4ce0a39e5
amyangfei <amyangfei@gmail.com>
2014-10-11 11:27:26 +0800

    Add counter example

---
+++ examples/counter.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"../redlock"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	fpath = "./counter.log"
+)
+
+func writer(count int, back chan string) {
+	lock, err := redlock.NewRedLock([]string{
+		"tcp://127.0.0.1:6379",
+		"tcp://127.0.0.1:6380",
+		"tcp://127.0.0.1:6381",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	incr := 0
+	for i := 0; i < count; i++ {
+		expiry, err := lock.Lock("foo", 1000)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			if expiry > 500 {
+				f, err := os.OpenFile(fpath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+				if err != nil {
+					panic(err)
+				}
+				defer f.Close()
+				buf := make([]byte, 1024)
+				n, _ := f.Read(buf)
+				num, _ := strconv.ParseInt(strings.TrimRight(string(buf[:n]), "\n"), 10, 64)
+				f.WriteAt([]byte(strconv.Itoa(int(num+1))), 0)
+				incr += 1
+				lock.UnLock()
+			}
+			// defer lock.UnLock()
+		}
+	}
+	fmt.Printf("%s increased %d times.\n", fpath, incr)
+	back <- "done"
+}
+
+func init() {
+	f, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	f.WriteString("0")
+	defer f.Close()
+}
+
+func main() {
+	threads := 5
+	inc := 100
+	done := make(chan string, threads)
+	for i := 0; i < threads; i++ {
+		go writer(inc, done)
+	}
+	for i := 0; i < threads; i++ {
+		<-done
+	}
+
+	f, err := os.OpenFile(fpath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	buf := make([]byte, 1024)
+	n, _ := f.Read(buf)
+	fmt.Printf("Counter value is %s\n", buf[:n])
+}
